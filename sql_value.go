50c437ddc7a5ee0c977d24103875f5d445f5913c
wildwind123 <34130873+wildwind123@users.noreply.github.com>
2023-08-31 12:18:15 +0900

    fix sql null (#8)
    Co-authored-by: Petr Zabolotskiy <z-petr-i@mail.ru>

--- sql_value.go
+++ sql_value.go
@@ -27,49 +27,49 @@ func (c *ConvertSqlValue) Set(value *ConvertValueParams) error {
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullFloat64":
-		v := sql.NullInt64{}
+		v := sql.NullFloat64{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullString":
-		v := sql.NullInt64{}
+		v := sql.NullString{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullInt32":
-		v := sql.NullInt64{}
+		v := sql.NullInt32{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullInt16":
-		v := sql.NullInt64{}
+		v := sql.NullInt16{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullByte":
-		v := sql.NullInt64{}
+		v := sql.NullByte{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullBool":
-		v := sql.NullInt64{}
+		v := sql.NullBool{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
 		}
 		value.ReflectValue.Set(reflect.ValueOf(v))
 	case "sql.NullTime":
-		v := sql.NullInt64{}
+		v := sql.NullTime{}
 		err = v.Scan(value.Items[value.Index])
 		if err != nil {
 			return errors.Wrap(err, "cant c.Value.Scan")
